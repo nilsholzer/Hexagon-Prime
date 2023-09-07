77dd25244f61871a3c054110cac39294612e0277
uhquw <uhquw@student.kit.edu>
2023-09-07 16:16:46 +0200

    ching chang chong

--- src/edu/kit/kastel/model/exceptions/BuildException.java
+++ src/edu/kit/kastel/model/exceptions/BuildException.java
@@ -1,6 +1,5 @@
 package edu.kit.kastel.model.exceptions;
 
-
 public class BuildException extends IllegalArgumentException {
     public BuildException(final String message) {
         super(message);
