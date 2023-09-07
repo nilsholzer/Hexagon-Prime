1b462dd7816e79fcc8f0ffb047e89f53a2323b98
uhquw <uhquw@student.kit.edu>
2023-09-07 16:13:07 +0200

    usnr

--- src/edu/kit/kastel/model/exceptions/BuildException.java
+++ src/edu/kit/kastel/model/exceptions/BuildException.java
@@ -1,5 +1,6 @@
 package edu.kit.kastel.model.exceptions;
 
+
 public class BuildException extends IllegalArgumentException {
     public BuildException(final String message) {
         super(message);
